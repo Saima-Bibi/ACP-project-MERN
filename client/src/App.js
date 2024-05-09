a262d1bfdc2c16744943a2d7db8662e7255af4cc
Saima Bibi <bibi33243@gmail.com>
2024-05-09 20:26:24 +0500

    HeroSection & footer created

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 
 import './App.css';
+import HeroSection from './components/HeroSection';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 
 function App() {
   return (
   <>
 <Navbar></Navbar>
+<HeroSection></HeroSection>
+<Footer></Footer>
   </>
   );
 }
---
+++ client/src/components/HeroSection.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import hero from '../images/hero.jpg'
+export default function HeroSection() {
+  return (
+    <>
+    
+      <div className='max-w-screen-2xl container mx-auto md:px-20 px-4 flex flex-col md:flex-row my-10'>
+        <div className='w-full  order-2 md:order-1 md:w-1/2 md:mt-16 mt-12 '>
+            <div className='space-y-12 '>
+            <h1 className='text-4xl font-bold'>Hello welcome here to learn something<span className='text-blue-500'> new everyday!!!</span></h1>
+            <p className='text-xm'>Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo. Nemo enim ipsam voluptatem, quia voluptas sit, aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos, qui ratione voluptatem sequi nesciunt, neque porro quisquam est, qui dolorem ipsum</p>
+            <label className="input input-bordered flex items-center gap-2">
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+  <input type="text" className="grow" placeholder="Email" />
+</label>
+</div>
+<button className="btn btn-primary mt-6">Primary</button>
+        </div>
+        <div className=' order-1 w-full md:w-1/2'>
+            <img src={hero}></img>
+        </div>
+      </div>
+      <hr></hr>
+    </>
+  )
+}
